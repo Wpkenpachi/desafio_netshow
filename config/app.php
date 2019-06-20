74cb885508ba79c0d2f59365e721ad750e74e03f
Wesley <angelportinari17@gmail.com>
2019-06-19 21:45:37 -0300

    env APP_DEBUG setado para true

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:CbamkuKi51oWk9j/5YvMjqagaIU99ld18m8Y7JcFOQ0='),
 
     'cipher' => 'AES-256-CBC',
 
